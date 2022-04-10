ff7fdcb66e044fc1dd4f7049042bd98fbe72f70b
Blockchain Dev <nevillebrown_930@hotmail.com>
2022-04-10 13:20:33 -0400

    Updated with a Loader Icon

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SwipeButtons from './SwipeButtons';
 
 
 function App() {
+  
   return (
     <div className="app">
       <Header/>
--- src/TinderCards.jsx
+++ src/TinderCards.jsx
@@ -5,15 +5,21 @@ import axios from './axios.js';
 
 
 
+
 function TinderCards() {
     const [people, setPeople] = useState([]);
+    const [loading, setLoading] = useState(true);
 
 //this connects the front & backend 
 
     useEffect(()=>{
+        
         async function fetchData() {
+            setLoading(true);
             const req = await axios.get('/tinder/cards');
             setPeople(req.data)
+            setLoading(false);
+            
         }
         fetchData(); 
     }, []);
@@ -33,17 +39,32 @@ function TinderCards() {
     
     return (
         <div className="tinderCards">
-            <div className="tinderCards__cardContainer">
+        
+        
+            <div 
+            className="tinderCards__cardContainer">
+
+            {loading && (<img
+              className="h-80 w-80 object-contain"
+              src="https://cdn.hackernoon.com/images/0*4Gzjgh9Y7Gu8KEtZ.gif"
+              alt=""
+            />)}
+
+            
             {people.map((person)=>(
+            
                 
                 <TinderCard
+                
                 className="swipe"
                 key={person.name}
                 preventSwipe={["up", "down"]}
                 onSwipe={(dir) => swiped(dir, person.name)}
                 onCardLeftScreen= {()=> outOfFrame(person.name)}
                 >
-                    <div style={{ backgroundImage: `url(${person.imgUrl})`}}
+                    <div 
+                
+                    style={{ backgroundImage: `url(${person.imgUrl})`}}
                     className="card">
                         <h3>{person.name}</h3>
                     </div>
@@ -51,6 +72,7 @@ function TinderCards() {
             )
                
             )}
+            
             </div>
 
            
